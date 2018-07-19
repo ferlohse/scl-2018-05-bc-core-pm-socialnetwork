5bfa6cc90e6f56bc113694fe0f4a0f0d79114d0a
SofiaMaturanaM <sofimatsu@gmail.com>
2018-07-19 16:52:52 -0400

    funciones declaradas TDD

---
+++ Timeline/timelineJS.js
@@ -0,0 +1,38 @@
+function newPost() {
+    //should add a new post to the timeline
+    //should not allow empty post
+}
+
+
+function newComment() {
+    //should add a new comment on a post
+
+}
+
+
+
+function editPost() {
+    //should edit post
+    //should save new version of post
+
+}
+
+
+
+function deletePost() {
+    //should delete a post after user confirmation
+}
+
+
+
+function countStars() {
+    //should count number of stars clicked per post
+    //should increase number of stars by 1 for every click of the icon
+}
+
+
+
+function displayStars() {
+    //should display number of stars clicked per post
+
+}
